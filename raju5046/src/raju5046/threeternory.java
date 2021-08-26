47b55629f79c20f8d76f5562df296ae212eeede4
Anupama34 <buridianupama123@gmail.com>
2021-08-26 23:15:26 +0530

    first commit

---
+++ raju5046/src/raju5046/threeternory.java
@@ -0,0 +1,12 @@
+package raju5046;
+import java.util.Scanner;
+public class threeternory{
+public static void main(String...args) {
+Scanner in=new Scanner(System.in);
+System.out.println("Enter two numbers :");
+int a=in.nextInt();
+int b=in.nextInt();
+int c=a>b?a:b;
+System.out.println(c+" is bigger");
+}
+}
